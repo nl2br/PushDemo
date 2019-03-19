e2a2d8f34e4c7b2a6db8fe67438e92e122cce7de
serviceinformatique <*serVedu35>
2019-03-19 16:30:45 +0100

    topic creation

--- app/src/main/java/fr/mds/nl/pushdemo/activity/MainActivity.java
+++ app/src/main/java/fr/mds/nl/pushdemo/activity/MainActivity.java
@@ -1,15 +1,37 @@
 package fr.mds.nl.pushdemo.activity;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import fr.mds.nl.pushdemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private static final String TAG = "mainactivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseMessaging.getInstance().subscribeToTopic("chat")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = "Subscribe success";
+                        if (!task.isSuccessful()) {
+                            msg = "Subscribe failure !!!";
+                        }
+                        Log.d(TAG, msg);
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
